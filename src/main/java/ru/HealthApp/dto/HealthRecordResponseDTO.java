3b42ead8d96109b765ec461a6e8dabd43adcbe4e
Blackened <blackened.07@mail.ru>
2026-03-04 13:18:50 +1000

    fixed HealthRecordService

---
+++ src/main/java/ru/HealthApp/dto/HealthRecordResponseDTO.java
@@ -0,0 +1,4 @@
+package ru.HealthApp.dto;
+
+public class HealthRecordResponseDTO {
+}
--- src/main/java/ru/HealthApp/service/HealthRecordService.java
+++ src/main/java/ru/HealthApp/service/HealthRecordService.java
@@ -1,22 +1,23 @@
 package ru.HealthApp.service;
 
+import ru.HealthApp.dto.HealthRecordRequestDTO;
+import ru.HealthApp.dto.HealthRecordResponseDTO;
+import ru.HealthApp.repository.entities.HealthMetricType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface HealthRecordService {
 
-    // Создание записи (за себя или за вирт)
     HealthRecordResponseDTO createRecord(Long authorId, Long targetId, HealthRecordRequestDTO data);
 
-    // 2. Чтение истории за период (с фильтром по типу)
     List<HealthRecordResponseDTO> getHistory(Long actorId, Long targetId, String type, LocalDateTime from, LocalDateTime to);
 
-    // 3. Сводка по семье (имя + дата + последний замер каждого участника)
     List<HealthRecordResponseDTO> getFamilyDashboard(Long adminId);
 
-    // 4. Редактирование записи
     HealthRecordResponseDTO updateRecord(Long actorId, Long recordId, HealthRecordRequestDTO newData);
 
-    // 5. Список всех типов по умолчанию (для UI)
     List<HealthMetricType> getDefaultMetrics();
-
-    // 6. Удаление записи
+    
     void deleteRecord(Long actorId, Long recordId);
 }
